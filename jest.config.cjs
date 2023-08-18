4091701b3d884dc55167ff75df4f34a973ab1974
LucasSantosOliveira2 <lucas-santos-2011@live.com>
2023-08-18 17:09:32 -0300

    feat:Favorite button was adjustded

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,5 +3,8 @@ module.exports = {
     testPathIgnorePatterns: ['/node_modules', '/.next'],
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.ts(x)'],
-    setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.ts']
-}
+    setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.ts'],
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/.jest/empty-module.ts"
+    }
+};
